65441feb0a5be0fbfc03df793d7bf09a093e4cf6
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-02-20 23:36:34 +0900

    Modify .gitignore

---
+++ app/src/main/java/com/inu555/ringmybell_rasberrypi_test_auto_ver/MainActivity.java
@@ -0,0 +1,14 @@
+package com.inu555.ringmybell_rasberrypi_test_auto_ver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
